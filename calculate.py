9d43ad526a9b755d69284255b41761aedc294105
Andy Liu <andyliu11037@gmail.com>
2020-10-02 22:29:58 +1000

    add calculate functions

---
+++ calculate.py
@@ -0,0 +1,11 @@
+def addition(self, input_num, calculator_num):
+    return input_num+calculator_num
+
+def subtraction(self, input_num, calculator_num):
+    return input_num-calculator_num
+
+def division(self, input_num, calculator_num):
+    return calculator_num/input_num
+    
+def multiplication(self, input_num, calculator_num):
+    return calculator_num*input_num
